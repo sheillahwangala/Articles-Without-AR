c63b708dba77fa3cab75beee915eb9c212897eba
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-30 22:39:20 +0300

    add method

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,14 +1,32 @@
+require_relative './article.rb'
+
 class Author
   attr_accessor :name
-
 
   def initialize(name)
     @name = name
   
   end
 
-  def author_name
+  def name
     @name
+  end
+
+  # to return articles by author
+  def articles
+    Article.all.select {|article| article.author.name == self.name}
+  end
+
+  # to return unique array of magazine instances by author
+  def magazines
+    @magazines=Article.all.select do |article|
+      if article.author.name == self.name
+        article.magazine
+      end
+    end
+    # to get unique magazines
+    @magazines.uniq
+
   end
 
 
